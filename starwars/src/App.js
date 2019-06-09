aed0829b9b72d56a26ae0f02850d8366bd0aa713
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-06-08 19:56:20 -0500

    Pagination System with Buttons added

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -23,24 +23,52 @@ class App extends Component {
         console.log(data);
         console.log('Next Page', data.next);
         this.setState({ starwarsChars: data.results });
-     
         //Pagination system that looping and loading all pages
-          if(data.next !== null) {
-             this.getCharacters(data.next)
-          } else if (data.next == null){
-            this.getCharacters('https://swapi.co/api/people/')
-          }
-          
+          // if(data.next !== null) {
+          //    this.getCharacters(data.next)
+          // } else if (data.next == null){
+          //   this.getCharacters('https://swapi.co/api/people/')
+          // }
       })
       .catch(err => {
         throw new Error(err);
       });
   };
 
+ 
+  //Pagination System with Buttons
+  page1 = () => {
+    let pageNumber = 1;
+   //  pageNumber++;
+    this.getCharacters( `https://swapi.co/api/people/?page=${pageNumber}`);
+  }
+
+  page1 = () => {
+    this.getCharacters( `https://swapi.co/api/people/?page=1`);
+  }
+  page2 = () => {
+     this.getCharacters( `https://swapi.co/api/people/?page=2`);
+   }
+  page3 = () => {
+     this.getCharacters( `https://swapi.co/api/people/?page=3`);
+   }
+  page4 = () => {
+     this.getCharacters( `https://swapi.co/api/people/?page=4`);
+   }
+  page5 = () => {
+     this.getCharacters( `https://swapi.co/api/people/?page=5`);
+   }
+
+
   render() {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <button onClick={this.page1}>Page 1</button>
+        <button onClick={this.page2}>Page 2</button>
+        <button onClick={this.page3}>Page 3</button>
+        <button onClick={this.page4}>Page 4</button>
+        <button onClick={this.page5}>Page 5</button>
         {this.state.starwarsChars.map((item, index) => {
           return <Star star={item} key={index} />
         })}
